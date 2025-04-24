23a9c040ed95edd7ec447453816fa255c128e5ad
venkatsundaraj <venkateshsundarasan@gmail.com>
2025-04-24 12:37:02 +0530

    redirection

--- next.config.mjs
+++ next.config.mjs
@@ -16,6 +16,21 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  async redirects() {
+    return [
+      {
+        source: "/(.*)",
+        has: [
+          {
+            type: "host",
+            value: "m.cosmostrategy.com",
+          },
+        ],
+        destination: "https://www.cosmostrategy.com/:1",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
